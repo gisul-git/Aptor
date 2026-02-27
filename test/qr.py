cb46552a2ae2a9e6142ffaf3d0ddf80b8b6dac0a
vijethpoojary <poojaryvijeth239@gmail.com>
2026-02-27 10:40:10 +0530

    Demo Service deployed in QA Debian fixed

--- test/qr.py
+++ test/qr.py
@@ -2,4 +2,4 @@ import pyotp
 secret = "5GCGWNN5NPT35VLPO64F77VXFIOUXEUG"
 totp = pyotp.TOTP(secret)
 uri = totp.provisioning_uri(name="Super Admin", issuer_name="AI Assessment Platform")
-print(uri)  # Use this to generate QR code
+print(uri)  # Use this to generate QR codes
